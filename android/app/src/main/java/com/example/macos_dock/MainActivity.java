b7c77e1baa3b2a701d062d8622b51707b6e33ef5
chetannager <chetan.nagar@weddingkart.co>
2024-12-22 00:42:49 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/macos_dock/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.macos_dock;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+/// Entrypoint of the application.
+void main() {
+  runApp(const MyApp());
+}
+
+/// [Widget] building the [MaterialApp].
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MacOs Dock",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          alignment: Alignment.bottomCenter,
+          margin: const EdgeInsets.only(bottom: 25.0),
+          child: Dock(
+            items: const [
+              Icons.person,
+              Icons.message,
+              Icons.call,
+              Icons.camera,
+              Icons.photo,
+            ],
+            builder: (e, isHovered, isDragging) {
+              return AnimatedScale(
+                duration: const Duration(milliseconds: 200),
+                scale: isHovered ? 1.4 : (isDragging ? 1.2 : 1.0),
+                child: Container(
+                  constraints: const BoxConstraints(minWidth: 48),
+                  height: 48,
+                  margin: const EdgeInsets.symmetric(horizontal: 8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color:
+                        Colors.primaries[e.hashCode % Colors.primaries.length],
+                    boxShadow: isHovered
+                        ? [
+                            const BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 12,
+                              offset: Offset(0, 8),
+                            ),
+                          ]
+                        : [],
+                  ),
+                  child: Center(child: Icon(e, color: Colors.white)),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// Dock of draggable [items].
+class Dock<T extends Object> extends StatefulWidget {
+  const Dock({
+    super.key,
+    this.items = const [],
+    required this.builder,
+  });
+
+  /// Initial [T] items to put in this [Dock].
+  final List<T> items;
+
+  /// Builder building the provided [T] item with states.
+  final Widget Function(T, bool isHovered, bool isDragging) builder;
+
+  @override
+  State<Dock<T>> createState() => _DockState<T>();
+}
+
+/// State of the [Dock] used to manipulate [_items].
+class _DockState<T extends Object> extends State<Dock<T>> {
+  /// [T] items being manipulated.
+  late final List<T> _items = widget.items.toList();
+
+  /// Tracks the currently dragging item.
+  T? _draggedItem;
+
+  /// Tracks the original index of the dragging item.
+  int? _draggedIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.black12,
+      ),
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          for (int i = 0; i <= _items.length; i++) ...[
+            MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: DragTarget<T>(
+                onWillAccept: (_) => true,
+                onAccept: (data) {
+                  setState(() {
+                    _items.insert(i, data);
+                    _draggedItem = null;
+                    _draggedIndex = null;
+                  });
+                },
+                builder: (context, candidateData, rejectedData) {
+                  return AnimatedContainer(
+                    duration: const Duration(milliseconds: 100),
+                    width: candidateData.isNotEmpty ? 56 : 16,
+                    height: 48,
+                  );
+                },
+              ),
+            ),
+            if (i < _items.length)
+              Draggable<T>(
+                data: _items[i],
+                dragAnchorStrategy: pointerDragAnchorStrategy,
+                feedback: Transform.scale(
+                  scale: 1.2,
+                  child: widget.builder(_items[i], false, true),
+                ),
+                onDragStarted: () {
+                  setState(() {
+                    _draggedItem = _items[i];
+                    _draggedIndex = i;
+                    _items.removeAt(i);
+                  });
+                },
+                onDragEnd: (details) {
+                  setState(() {
+                    if (_draggedItem != null && _draggedIndex != null) {
+                      _items.insert(_draggedIndex!, _draggedItem!);
+                    }
+                    _draggedItem = null;
+                    _draggedIndex = null;
+                  });
+                },
+                child: MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: DragTarget<T>(
+                    onWillAccept: (_) => false,
+                    builder: (context, candidateData, rejectedData) {
+                      return widget.builder(_items[i], false, false);
+                    },
+                  ),
+                ),
+              ),
+          ],
+        ],
+      ),
+    );
+  }
+}
